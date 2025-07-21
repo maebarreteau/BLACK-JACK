5e4596c8ba62d1c3308d4c8a479d493196b32822
mathiasmenotarg <maelysbarreteau23@gmail.com>
2025-07-22 00:27:55 +0200

    update memories

--- memories.js
+++ memories.js
@@ -4,6 +4,7 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
@@ -17,9 +18,11 @@ function flipCard() {
     return;
   }
 
+  
   secondCard = this;
   checkForMatch();
 }
+
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
@@ -28,26 +31,37 @@ function checkForMatch() {
 }
 
 function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.add('disappear');
+    secondCard.classList.add('disappear');
+    resetBoard();
+  }, 500); 
 }
+
 
 function unflipCards() {
   lockBoard = true;
-
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
   }, 1000);
 }
+
 
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
+
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * cards.length);
+    card.style.order = randomPos;
+  });
+})();
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
